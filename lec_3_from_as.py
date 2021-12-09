09b8cc320e048fb8974eb55a42de687ba754f045
Copy11 <92929980+Copy1@users.noreply.github.com>
2021-12-09 15:49:34 +0000

    jhfjy

---
+++ lec_3_from_as.py
@@ -0,0 +1,11 @@
+from lec_3_my_module import earth_mass as em
+from lec_3_my_module import sigma_steff_bolc as G
+from lec_3_my_module import gravity_constant as sigma
+x = em * G * sigma
+print(x)
+
+import math
+a  = math.sin(3 * math.pi / 5)
+print(a)
+b = math.sqrt(3)
+print(b)
---
+++ lec_3_generators.py
@@ -0,0 +1,7 @@
+import numpy as np 
+a = range(0, 5, 11)
+print(a)
+b = np.arange(0, 5, 0.1)
+print(b)
+d = np.linspace(0, 5, 12)
+print(d)
