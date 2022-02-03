8a9f0521b4cfd233f0483c99263a07504134769b
Copy11 <92929980+Copy1@users.noreply.github.com>
2022-02-03 15:37:21 +0000

    klgll

--- lab_4_task_1.py
+++ lab_4_task_1.py
@@ -1,22 +1,4 @@
-#  from random import random
-# N = 10
- 
-# def average(a):
-#     s = 0
-#     for i in range(N):
-#         s += a[i]
-#     return s/N
- 
-# arr = [0] * N
-# for i in range(N):
-#     arr[i] = int(random() * 100)
 
-# b = average(arr)
-# print(arr)
-# print(b)
-
-# L = [1, 7, 9, 90]
-# arf_funk(L)
 
 def zoi_funс(L):
   
@@ -29,9 +11,3 @@ def zoi_funс(L):
 L = [1, 7, 9, 90]
 print(zoi_funс(L))
 
- 
-
-
-
-
- 
