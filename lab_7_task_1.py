85ce67d7b5a04846d962711607e7b2cdadd2e7d0
Copy11 <92929980+Copy1@users.noreply.github.com>
2022-03-10 16:21:38 +0000

    pjgvpii

---
+++ lab_7_task_1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+R = 100
+t = np.arange(-2*np.pi, 2*np.pi, 0.1)
+
+x = R * (t - (np.sin(t)))
+y = R * (1 - (np.cos(t)))
+
+plt.plot(x, y, ls = '-')
+plt.axis('equal')
+plt.show()
+
+
